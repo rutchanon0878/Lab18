8c33951f94de1eb961e9cb0120004e36c5e95461
rutchanon0878 <rutchanon_inkam@cmu.ac.th>
2024-02-09 17:41:10 +0700

    finish

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1,Rect R2){
+	double A,B,C;
+	A=min(R1.x+R1.w,R2.x+R2.w)-max(R1.x,R2.x);
+	B=min(R1.y,R2.y)-max(R1.y-R1.h,R2.y-R2.h);
+	if(A<0 || B<0) return 0;
+	C=A*B;
+	return C;
 }
